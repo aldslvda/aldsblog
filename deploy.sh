7a025f79271dde1c66e817f7313cb1640abc9eca
NickAlds <aldslvda@live.com>
2019-04-25 09:52:02 +0800

    add blog src

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cd aldsblog
+git pull
+cd ../
+cp aldsblog/content/post_hexo/* source/_posts/
+hexo clean
+hexo g
+hexo d
+
+
+
---
+++ source/categories/index.md
@@ -0,0 +1,5 @@
+---
+title: categories
+date: 2018-07-29 18:39:32
+type: "categories"
+---
